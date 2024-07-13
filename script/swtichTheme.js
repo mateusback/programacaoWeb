2996b896c9e382c5082ccc4b27688eaeb4483af4
Mateus Back <m.back@nextage.com.br>
2024-07-13 12:18:57 -0300

    Update layout, switch theme functionality, and add lazy loading for movie images

--- script/swtichTheme.js
+++ script/swtichTheme.js
@@ -1,27 +1,12 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-function switchTheme(e) {
-    checkSavedTheme();
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {
+function switchTheme() {
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+    
+    if (currentTheme === 'dark') {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     }    
 }
-
-function checkSavedTheme(){
-    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-
-    if (currentTheme) {
-        document.documentElement.setAttribute('data-theme', currentTheme);
-
-        if (currentTheme === 'dark') {
-            toggleSwitch.checked = true;
-        }
-    }
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
