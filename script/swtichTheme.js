6ad4be52d4eeffd2341c058631fdcc4724f29a33
Mateus Back <m.back@nextage.com.br>
2024-07-13 10:55:23 -0300

    feat: update layout, and switch theme functionality

---
+++ script/swtichTheme.js
@@ -0,0 +1,27 @@
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    checkSavedTheme();
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }    
+}
+
+function checkSavedTheme(){
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+
+        if (currentTheme === 'dark') {
+            toggleSwitch.checked = true;
+        }
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
